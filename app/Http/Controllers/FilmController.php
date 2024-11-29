45c25439917603cf77e4dfcd391051e69321df1b
perita23 <hugobedmar10@gmail.com>
2024-11-29 19:23:49 +0100

    #3 done

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -9,7 +9,8 @@ class FilmController extends Controller
     /**
      * Read films from storage
      */
-    public static function readFilms(): array {
+    public static function readFilms(): array
+    {
         $films = Storage::json('/public/films.json');
         return $films;
     }
@@ -18,16 +19,16 @@ class FilmController extends Controller
      * if year is not infomed 2000 year will be used as criteria
      */
     public function listOldFilms($year = null)
-    {        
+    {
         $old_films = [];
         if (is_null($year))
-        $year = 2000;
-    
-        $title = "Listado de Pelis Antiguas (Antes de $year)";    
+            $year = 2000;
+
+        $title = "Listado de Pelis Antiguas (Antes de $year)";
         $films = FilmController::readFilms();
 
         foreach ($films as $film) {
-        //foreach ($this->datasource as $film) {
+            //foreach ($this->datasource as $film) {
             if ($film['year'] < $year)
                 $old_films[] = $film;
         }
@@ -55,27 +56,48 @@ class FilmController extends Controller
     /**
      * Lista TODAS las películas o filtra x año o categoría.
      */
-    public function listFilms($year = null, $genre = null)
+    public function listFilmsByYear($year = null)
     {
         $films_filtered = [];
 
         $title = "Listado de todas las pelis";
         $films = FilmController::readFilms();
 
-        //if year and genre are null
-        if (is_null($year) && is_null($genre))
-            return view('films.list', ["films" => $films, "title" => $title]);
+        if (isset($_GET["year"])) {
+            $year = $_GET["year"];
+        }
 
+        //if year and genre are null
+        if (is_null($year))
+            return view('films.list', ["films" => $films, "title" => $title]);
         //list based on year or genre informed
         foreach ($films as $film) {
-            if ((!is_null($year) && is_null($genre)) && $film['year'] == $year){
+            if ((!is_null($year)) && $film['year'] == $year) {
                 $title = "Listado de todas las pelis filtrado x año";
                 $films_filtered[] = $film;
-            }else if((is_null($year) && !is_null($genre)) && strtolower($film['genre']) == strtolower($genre)){
-                $title = "Listado de todas las pelis filtrado x categoria";
-                $films_filtered[] = $film;
-            }else if(!is_null($year) && !is_null($genre) && strtolower($film['genre']) == strtolower($genre) && $film['year'] == $year){
-                $title = "Listado de todas las pelis filtrado x categoria y año";
+            }
+        }
+        return view("films.list", ["films" => $films_filtered, "title" => $title]);
+    }
+
+    public function listFilmsByGenre($genre = null)
+    {
+        $films_filtered = [];
+
+        $title = "Listado de todas las pelis";
+        $films = FilmController::readFilms();
+
+        if (isset($_GET["genre"])) {
+            $genre = $_GET["genre"];
+        }
+
+        //if year and genre are null
+        if (is_null($genre))
+            return view('films.list', ["films" => $films, "title" => $title]);
+        //list based on year or genre informed
+        foreach ($films as $film) {
+            if ((!is_null($genre)) && $film['genre'] == $genre) {
+                $title = "Listado de todas las pelis filtrado x año";
                 $films_filtered[] = $film;
             }
         }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,8 @@
     <ul>
         <li><a href=/filmout/oldFilms>Pelis antiguas</a></li>
         <li><a href=/filmout/newFilms>Pelis nuevas</a></li>
-        <li><a href=/filmout/films>Pelis</a></li>
+        <li><a href=/filmout/filmsByYear>Pelis por año</a></li>
+        <li><a href=/filmout/filmsByGenre>Pelis por genero</a></li>
     </ul>
     <!-- Add Bootstrap JS and Popper.js (required for Bootstrap) -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
