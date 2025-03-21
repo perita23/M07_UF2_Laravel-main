09fd1025c3d883aaca10fbdc22f4c66e749a110d
Hugo Gonzalez <hugobedmar10@gmail.com>
2025-03-21 18:27:35 +0100

    Add initial files for hot reload, npm ignores, and lodash function exports

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\ActorController;
-use App\Http\Controllers\FilmController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -10,8 +9,8 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
@@ -19,26 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware('year')->group(function () {
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-    Route::group(['prefix' => 'filmout'], function () {
-        // Routes included with prefix "filmout"
-        Route::get('oldFilms/{year?}', [FilmController::class, "listOldFilms"])->name('oldFilms');
-        Route::get('newFilms/{year?}', [FilmController::class, "listNewFilms"])->name('newFilms');
-        Route::get('filmsByYear/{year?}', [FilmController::class, "listFilmsByYear"])->name('filmsByYear');
-        Route::get('filmsByGenre/{genre?}', [FilmController::class, "listFilmsByGenre"])->name('filmsBygenre');
-        Route::get('filmsSorted', [FilmController::class, "listSortedFilms"])->name('filmsSorted');
-        Route::get('countFilms', [FilmController::class, "countFilms"])->name('countFilms');
-    });
-    Route::group(['prefix' => 'filmin'], function () {
-        Route::post('/createFilm', [FilmController::class, "createFilm"])->name("createFilm")->middleware('validate.url');
-        Route::get('addFilm', function () {
-            return view('films.addFilm', ["exist" => false, "invalidUrl" => false]);
-        })->name('addFilmForm');
-    });
-    Route::group(['prefix' => 'actors'], function () {
-        Route::get('countActors', [ActorController::class, "countActors"])->name("countActors");
-        Route::get('listActors/{deacade?}', [ActorController::class, "listActor"])->name("listActor");
-        
-    });
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
